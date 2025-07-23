3e4b58aed1a245712c2e895719dc981f08dd99bc
SanjuShree1910 <131963539+SanjuShree1910@users.noreply.github.com>
2025-07-23 23:40:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
